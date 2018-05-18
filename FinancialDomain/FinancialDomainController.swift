f9f3a3237cdfd25d038506711a6ffcc82616e7f4
Vikas Ahuja <vahuja4@sapient.com>
2018-05-18 12:51:06 +0530

    add new file financial domain

---
+++ FinancialDomain/FinancialDomainController.swift
@@ -0,0 +1,14 @@
+//
+//  FinancialDomainController.swift
+//  FinancialDomain
+//
+//  Created by Vikas Ahuja on 5/18/18.
+//  Copyright © 2018 Vikas Ahuja. All rights reserved.
+//
+
+import Foundation
+
+class FinancialDomainController {
+    //This is just to test finanicla domain controller
+    
+}
