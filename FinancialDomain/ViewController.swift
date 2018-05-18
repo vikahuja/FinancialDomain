7ad5749cae8cb896e790629121b924d81b27eb56
Vikas Ahuja <vahuja4@sapient.com>
2018-05-18 12:23:40 +0530

    commit controller changes

--- FinancialDomain/ViewController.swift
+++ FinancialDomain/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Print statement for test in view controller
         // Do any additional setup after loading the view, typically from a nib.
     }
 
