3277fba9dd0d87c915e6bde606881e1c9883db84
Vikas Ahuja <vahuja4@sapient.com>
2018-05-18 15:29:10 +0530

    commit changes

--- FinancialDomainUITests/FinancialDomainUITests.swift
+++ FinancialDomainUITests/FinancialDomainUITests.swift
@@ -12,7 +12,7 @@ class FinancialDomainUITests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
+        //test
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
